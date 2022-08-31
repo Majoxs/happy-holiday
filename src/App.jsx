0eff9108c4d786a799a32c2d562f50e025d8bbcd
Majoxs <majoxs@gmail.com>
2022-08-31 17:54:36 +0500

    useContext and useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Card from './components/Card/Card';
+import { TextContextProvider } from './context/textContext';
+import { ImgContextProvider } from './context/imgContext';
+import { HolidayContextProvider } from './context/holidayContext';
 
 const wrapper = {
   display: 'flex',
@@ -11,11 +14,15 @@ const wrapper = {
 const App = () => {
   return (
     <div style={wrapper}>
-      <Header />
-      <Card />
-      
-
-      <Footer />
+      <HolidayContextProvider>
+        <ImgContextProvider>
+          <TextContextProvider>
+            <Header />
+            <Card />
+            <Footer />
+          </TextContextProvider>
+        </ImgContextProvider>
+      </HolidayContextProvider>
     </div>
   );
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,3 @@
-import Container from '../Container/Container';
 import style from './Card.module.css';
 import Felicitation from './Felicitation/Felicitation';
 import ImageCard from './ImageCard/ImageCard';
@@ -6,16 +5,14 @@ import CardBG from '../../img/card-bg.jpg';
 
 
 const Card = () => (
-  <Container>
-    <div className={style.card}>
-      <div className={style.wrapper}>
-        <div className={style.image}>
-          <ImageCard img={CardBG} />
-          <Felicitation />
-        </div>
+  <div className={style.card}>
+    <div className={style.wrapper}>
+      <div className={style.image}>
+        <ImageCard img={CardBG} />
+        <Felicitation />
       </div>
     </div>
-  </Container>
+  </div>
 );
 
 export default Card;
